2289768dc4f91fd144f69895fb7dc60392b5d660
Warren Li <warrenli@dontrush.org>
2009-02-01 22:29:43 +0800

    Update REAME file

--- config/environment.rb
+++ config/environment.rb
@@ -92,4 +92,7 @@ Rails::Initializer.run do |config|
   # Activate observers that should always be running
   # Please note that observers generated using script/generate observer need to have an _observer suffix
   # config.active_record.observers = :cacher, :garbage_collector, :forum_observer
+
+  # remove newline after erb block
+  config.action_view.erb_trim_mode = ">"
 end

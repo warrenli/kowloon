fe5d353a31a71f94df59668a2343c10628ed1c2c
Warren Li <warrenli@dontrush.org>
2009-03-30 18:07:04 +0800

    Use ActiveRecord session store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -5,11 +5,11 @@
 # Make sure the secret is at least 30 characters and all random, 
 # no regular words or you'll be exposed to dictionary attacks.
 ActionController::Base.session = {
-  :key         => '_rails232_session',
+  :key         => '_kowloon_session',
   :secret      => '2279889202252738dc9ec2d893b2cbdafcb0b74953d34811cf1653dde43fb2af05a8938cd13adf45f264e26df040b33c7ba0ba5ba2aa87d8e03766595880de8e'
 }
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rake db:sessions:create")
-# ActionController::Base.session_store = :active_record_store
+ActionController::Base.session_store = :active_record_store
